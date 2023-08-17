b193d6786bf4d8e108fdde7470a688ec57964797
Luuchinh19 <Phuongchinhvn2020@gmail.com>
2023-08-17 17:04:47 +0700

    commit Controllers(Web)

---
+++ app/Controllers/Web/PageController.php
@@ -0,0 +1,10 @@
+<?php
+require_once('app/Controllers/Web/WebController.php');
+
+class PageController extends WebController
+{
+    public function lien_he()
+    {
+        return $this->view('pages/lien_he.php');
+    }
+}
